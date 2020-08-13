0d8aa6780b180dcd19f28af77a65766d046ee96a
Jyotsna <jyotsna.ratak@gmail.com>
2020-08-12 22:50:27 -0500

    transaction rewards

--- src/test/java/com/example/service/springbootrewards/SpringBootRewardsApplicationTests.java
+++ src/test/java/com/example/service/springbootrewards/SpringBootRewardsApplicationTests.java
@@ -1,18 +1,14 @@
 package com.example.service.springbootrewards;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SpringBootRewardsApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
--- src/test/java/com/example/service/springbootrewards/rewards/RewardsControllerTest.java
+++ src/test/java/com/example/service/springbootrewards/rewards/RewardsControllerTest.java
@@ -1,45 +1,37 @@
 package com.example.service.springbootrewards.rewards;
 
 import com.trans.service.springbootrewards.model.Customer;
-import com.trans.service.springbootrewards.rewards.CustomerRepository;
 import com.trans.service.springbootrewards.rewards.RewardsService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@ExtendWith(MockitoExtension.class)
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
 public class RewardsControllerTest {
-	
-	@InjectMocks
-	RestController restController;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private RewardsService service;
+
 	@Mock
-	RewardsService rewardsService;
-	@Mock
-	List<Customer> customerList;
+	List<Customer> customerLists;
 
 	@Test
-	public void testCustomers()
-	{
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-		when(rewardsService.getCustomerAll()).thenReturn(customerList);
-
-
-
+	public void getCustomers() throws Exception {
+		when(service.getCustomerAll()).thenReturn(customerLists);
+		this.mockMvc.perform(get("/customers")).andDo(print()).andExpect(status().isOk());
 	}
-	
 }
